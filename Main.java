af7ccaf22756ab41ad6cbe4faa36ed71762ee613
JustinChan <justinc884@nycstudents.net>
2025-09-15 15:17:31 -0400

    Here is the feeder code.

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
-        Feeder f = new Feeder();
+public class Main {
+    public static void main(String[] args) {
+        Feeder f = new Feeder(500);
+        System.out.println(f);
+        System.out.println("Current Food: " + f.getCurrentFood());
+        f.simulateOneDay(12);
+        System.out.println("Current Food: " + f.getCurrentFood());
+        f = new Feeder(1000);
+        f.simulateOneDay(22);
+        System.out.println("Current Food: " + f.getCurrentFood());
+        f = new Feeder(2400);
+        System.out.println(f.simulateManyDays(10, 4) + " days");
     }
+
 }
